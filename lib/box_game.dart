77ad6b2f6eaf6461f25653b03e0ab140c83025fa
阿航 <harrisonqi@foxmail.com>
2020-06-12 14:51:41 +0800

    :bookmark: 完成游戏!

--- lib/box_game.dart
+++ lib/box_game.dart
@@ -1,9 +1,23 @@
 import 'dart:ui';
 
 import 'package:flame/game.dart';
+import 'package:flame/gestures.dart';
+import 'package:flutter/gestures.dart';
 
-class BoxGame extends Game {
+class BoxGame extends Game with TapDetector {
   Size screenSize;
+  bool hasWon = false;
+
+  void onTapDown(TapDownDetails details) {
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    if (details.globalPosition.dx >= screenCenterX - 75 &&
+        details.globalPosition.dx <= screenCenterX + 75 &&
+        details.globalPosition.dy >= screenCenterY - 75 &&
+        details.globalPosition.dy <= screenCenterY + 75) {
+      hasWon = true;
+    }
+  }
 
   void resize(Size size) {
     screenSize = size;
@@ -12,8 +26,7 @@ class BoxGame extends Game {
 
   void render(Canvas canvas) {
     // 绘制背景
-    Rect bgRect = Rect.fromLTWH(0, 0,
-        screenSize.width, screenSize.height);
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
     Paint bgPaint = Paint();
     bgPaint.color = Color(0xFF000000);
     canvas.drawRect(bgRect, bgPaint);
@@ -21,14 +34,15 @@ class BoxGame extends Game {
     // 绘制"目标方块"
     double screenCenterX = screenSize.width / 2;
     double screenCenterY = screenSize.height / 2;
-    Rect boxRect = Rect.fromLTWH(
-        screenCenterX - 75,
-        screenCenterY - 75,
-        150,
-        150
-    );
+    Rect boxRect =
+        Rect.fromLTWH(screenCenterX - 75, screenCenterY - 75, 150, 150);
     Paint boxPaint = Paint();
-    boxPaint.color = Color(0xffffffff);
+    // 判断: 若游戏胜利, 方块变为绿色, 反之则为白色
+    if (hasWon) {
+      boxPaint.color = Color(0xff00ff00);
+    } else {
+      boxPaint.color = Color(0xffffffff);
+    }
     canvas.drawRect(boxRect, boxPaint);
   }
 
@@ -36,5 +50,3 @@ class BoxGame extends Game {
     // TODO: 实现 update
   }
 }
-
-
